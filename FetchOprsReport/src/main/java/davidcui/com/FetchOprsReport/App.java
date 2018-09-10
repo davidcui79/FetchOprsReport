8fb4e1d57a76fe3139a5028e0a2147a54affcc16
edahcui <david_cui79@yahoo.com>
2018-09-10 14:54:59 -0700

    Initial project creation

---
+++ FetchOprsReport/src/main/java/davidcui/com/FetchOprsReport/App.java
@@ -0,0 +1,13 @@
+package davidcui.com.FetchOprsReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
